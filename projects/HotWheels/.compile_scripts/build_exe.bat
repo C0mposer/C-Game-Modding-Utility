cc1557928ea071ce9c56f4f95bcd8313267b893e
C0mposer <78276528+C0mposer@users.noreply.github.com>
2023-12-15 15:43:33 -0800

    Delete projects directory

--- projects/HotWheels/.compile_scripts/build_exe.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd C:\Users\torbo\OneDrive\Documents\My-Documents\Coding\Python\Code Injection Toolchain\projects\HotWheels
-"../../prereq/cmd-compile/cmd_compile.exe" HotWheels Build_EXE
